d1a4c306e644eeab128cb6310b5706475802f211
Nguyen Cao Tuan <nguyencaotuan2004@gmail.com>
2025-05-08 13:17:13 +0700

    build(sentry): Add sentry to FE

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import * as Sentry from "@sentry/react-native";
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
+      <Button title='Try!' onPress={ () => { Sentry.captureException(new Error('First error')) }}/>
     </View>
   );
 };
